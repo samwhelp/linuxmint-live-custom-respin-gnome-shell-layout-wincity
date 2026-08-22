cd2aa754f227a58ce0040af63f26949cf73d1796
samwhelp <samwhelp@users.noreply.github.com>
2026-08-22 11:11:28 +0800

    fix

---
+++ template/hook/mods/desktop-environment-gnome-shell-extensions-install/sub/init.sh
@@ -0,0 +1,15 @@
+
+
+## sub / core
+. "${BASE_DIR_PATH}/sub/core.sh"
+
+
+
+## sub / args
+. "${BASE_DIR_PATH}/sub/args.sh"
+
+
+
+## sub / model
+. "${BASE_DIR_PATH}/sub/gnome-shell-extensions-install.sh"
+
